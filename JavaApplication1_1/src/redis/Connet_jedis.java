14c507801e08c520a217ec81271199ef65839f99
thuong1207 <chithuong.do@gmail.com>
2016-10-04 10:09:10 +0700

    dd

--- JavaApplication1_1/src/redis/Connet_jedis.java
+++ JavaApplication1_1/src/redis/Connet_jedis.java
@@ -6,6 +6,9 @@
 package redis;
 import java.io.File;
 import java.io.IOException;
+
+import org.xerial.snappy.Snappy;
+
 import redis.clients.jedis.Jedis;
 
 
@@ -15,25 +18,41 @@ import redis.clients.jedis.Jedis;
  */
 public class Connet_jedis {
     public static void main(String[] args) throws IOException, Exception {
+
+       // String path = "/home/thuongdc/Downloads/dumpdb";
+       //System.out.println(path);
+       // File f = new File(path);  
        // String path = "/home/thuongdc/Downloads/dump (5).rdb";
        //System.out.println(path);
        // File f = new File(path);
+
        String url = "localhost";
        String check ;
        Jedis jedis = new Jedis(url);
        String key = "name";
-       String value = "thuongdc";
-       byte[] keys = key.getBytes();
-       byte[] names = value.getBytes();
-      
-       //save in dump.rdb  
-       //jedis.flushAll();   
-       jedis.set(keys, names);
+       String value = "thuongdcsadadsdsafgdsgfdsgfdgfdgggggggggggggggggggdsfdsfdsfdsfdsffdsfgggg";
+
+
+
+       //Nen data
+       byte[]  KeySnappy  = Snappy.compress(value.getBytes());
+       byte[] ValueSnappy = Snappy.compress(value.getBytes());
+  
+       //SAVE
+       jedis.set(KeySnappy, ValueSnappy);
        jedis.save();
        
-       //parser rdb
+       byte[] NameDecoder = Snappy.uncompress(jedis.get(KeySnappy));
+       String result = new String(NameDecoder, "UTF-8");
+       System.out.println(result);
+       System.out.println(value.getBytes());
+       System.out.println(ValueSnappy);
+       System.out.println("...............................................");
+ 
        File f = new File("/var/lib/redis/6379/dump.rdb");     
        f.createNewFile();
-       RdbFilePrinter.printRdbFile(f);
+       
+       
+       //RdbFilePrinter.printRdbFile(f);
     }
 }
